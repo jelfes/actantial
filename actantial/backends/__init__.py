84f3dd16c1b5a161999f1934fdde59bea5fbd91d
jelfes <janelfes@gmail.com>
2026-01-28 16:00:29 +0100

    add init for backend folder to improve name resolution

---
+++ actantial/backends/__init__.py
@@ -0,0 +1,7 @@
+from .base import LLMBackend
+from .openai import OpenAIBackend
+from .anthropic import AnthropicBackend
+from .huggingface import HuggingFaceBackend
+
+# export backend classes for easy import
+__all__ = ["LLMBackend", "OpenAIBackend", "HuggingFaceBackend", "AnthropicBackend"]
